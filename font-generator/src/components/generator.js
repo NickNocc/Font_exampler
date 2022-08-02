57490ee833cd99e3697fa7af8976ec635a9768b4
NickNocc <haretakis4@gmail.com>
2022-08-02 12:06:13 -0700

    Font Selector

--- font-generator/src/components/generator.js
+++ font-generator/src/components/generator.js
@@ -1,18 +1,46 @@
 import React, { useState } from "react";
-import "./Generator.css"
+import "./Generator.css";
 
-export const Generator = ({}) => {  
-  const currentFont = [1, 2, 3, 4];
+export const Generator = () => {
+  const currentFont = [
+    "Aurora",
+    "Baked Fresh",
+    "Carefree",
+    "Clocks",
+    "Lively",
+    "Lobster",
+    "Ghisella",
+    "Violetta",
+    "Blossom",
+    "Chakra",
+    "Tatertot",
+    "Langrish",
+    "Classico",
+    "Hancock",
+    "Homework",
+    "Cookie",
+    "Amazingly beautiful",
+    "Eduarda",
+  ];
 
-  const [cF, setCf] = useState(currentFont)
+  const [cF, setCf] = useState(currentFont[0]);
   console.log(cF);
+
+  const handleFontChange = (event) => {
+    setCf(event.target.value);
+  };
 
   return (
     <div className="split-holder">
       <div className="split-left">
         <input type="text" id="name-input" placeholder="Enter Name" />
         <button id="submit-btn">Submit</button>
-        <select className="font-select" value={cF}></select>
+        <select className="font-select" value={cF} onChange={handleFontChange}>
+          {currentFont 
+          ? currentFont.map((font, i) => (
+            <option value={i} key={i}>{font}</option>
+          )) : null}
+        </select>
         {/* add font choices, maybe open select box instead */}
       </div>
       <div className="split-right">
