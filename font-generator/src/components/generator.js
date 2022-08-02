7b1de519baf5e8d539b99e1ad6d0b3acf0f995ea
NickNocc <haretakis4@gmail.com>
2022-08-02 14:27:41 -0700

    Added fonts, now troubleshooting

--- font-generator/src/components/generator.js
+++ font-generator/src/components/generator.js
@@ -3,31 +3,84 @@ import "./Generator.css";
 
 export const Generator = () => {
   const currentFont = [
-    "Aurora",
-    "Baked Fresh",
-    "Carefree",
-    "Clocks",
-    "Lively",
-    "Lobster",
-    "Ghisella",
-    "Violetta",
-    "Blossom",
-    "Chakra",
-    "Tatertot",
-    "Langrish",
-    "Classico",
-    "Hancock",
-    "Homework",
-    "Cookie",
-    "Amazingly beautiful",
-    "Eduarda",
+    {
+      fontName: "Amazingly beautiful",
+      fontClass: "font-face-Amazingly",
+    },
+    {
+      fontName: "Aurora",
+      fontClass: "font-face-aurora",
+    },
+    {
+      fontName: "Baked Fresh",
+      fontClass: "font-face-bakery",
+    },
+    {
+      fontName: "Blossom",
+      fontClass: "font-face-blossom",
+    },
+    {
+      fontName: "Carefree",
+      fontClass: "font-face-Carefree",
+    },
+    {
+      fontName: "Chakra",
+      fontClass: "font-face-Chakra",
+    },
+    {
+      fontName: "Classico",
+      fontClass: "font-face-Classico",
+    },
+    {
+      fontName: "Clocks",
+      fontClass: "font-face-Clocks",
+    },
+    {
+      fontName: "Cookie",
+      fontClass: "font-face-Cookie",
+    },
+    {
+      fontName: "Ghisella",
+      fontClass: "font-face-Ghisella",
+    },
+    {
+      fontName: "Hancock",
+      fontClass: "font-face-Hancock",
+    },
+    {
+      fontName: "Homework",
+      fontClass: "font-face-Homework",
+    },
+    {
+      fontName: "Langrish",
+      fontClass: "font-face-Langrish",
+    },
+    {
+      fontName: "Lively",
+      fontClass: "font-face-Lively",
+    },
+    {
+      fontName: "Lobster",
+      fontClass: "font-face-Lobster",
+    },
+    {
+      fontName: "Tatertot",
+      fontClass: "font-face-Tatertot",
+    },
+    {
+      fontName: "Violetta",
+      fontClass: "font-face-Violetta",
+    },
+    {
+      fontName: "Eduarda",
+      fontClass: "font-face-Eduarda",
+    },
   ];
 
   const [cF, setCf] = useState(currentFont[0]);
-  console.log(cF);
 
   const handleFontChange = (event) => {
-    setCf(event.target.value);
+    setCf(currentFont[event.target.value]);
   };
 
   return (
@@ -35,11 +88,15 @@ export const Generator = () => {
       <div className="split-left">
         <input type="text" id="name-input" placeholder="Enter Name" />
         <button id="submit-btn">Submit</button>
-        <select className="font-select" value={cF} onChange={handleFontChange}>
+        <select
+          className={"font-select"}
+          value={cF.fontName}
+          onChange={handleFontChange}
+        >
           {currentFont
             ? currentFont.map((font, i) => (
                 <option value={i} key={i}>
-                  {font}
+                  {font.fontName}
                 </option>
               ))
             : null}
@@ -47,7 +104,9 @@ export const Generator = () => {
         {/* add font choices, maybe open select box instead */}
       </div>
       <div className="split-right">
-        <div className=".font-face-aurora">Output</div>
+        <div className={cF.fontClass}>Output</div>
+        {console.log("font class: ", cF.fontClass)}
+        {console.log("font name: ", cF.value)}
         {/* update text with entered name */}
       </div>
     </div>
