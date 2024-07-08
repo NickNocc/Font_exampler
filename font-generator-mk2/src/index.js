67a872df902ce07cf21c34e6d1f1b670a075af7d
NickNocc <haretakis4@gmail.com>
2024-07-08 10:13:26 -0700

    removing old fonts

--- font-generator-mk2/src/index.js
+++ font-generator-mk2/src/index.js
@@ -22,11 +22,9 @@ import "./fonts/Eduarda.ttf"
 import "./fonts/Ghisella.ttf";
 import "./fonts/Hancock.ttf";
 import "./fonts/Harpsichord.ttf"
-// import "./fonts/Homework.ttf";
 import "./fonts/Langrish.ttf";
 import "./fonts/Layla.ttf";
 import "./fonts/Lively.ttf";
-// import "./fonts/Lobster.ttf";
 import "./fonts/Sweetheart.ttf";
 import "./fonts/Tatertot.ttf";
 import "./fonts/Violetta.ttf";
