6f9d6fdc5b0a42be4edd751ec3a1e81e24b1d1a4
NickNocc <haretakis4@gmail.com>
2024-08-05 11:14:06 -0700

    added a color selector

--- font-generator-mk2/src/components/generator.js
+++ font-generator-mk2/src/components/generator.js
@@ -163,15 +163,184 @@ export const Generator = () => {
     },
   ];
 
+  const currentColor = [
+    // {
+    //   color: "",
+    //   value: "",
+    //   label: "",
+    // },
+    {
+      color: "rgb(0, 0, 0)",
+      value: "Black",
+      label: "Black",
+    },
+    {
+      color: "rgb(255, 255, 255)",
+      value: "White",
+      label: "White",
+    },
+    {
+      color: "rgb(255, 255, 240)",
+      value: "Ivory",
+      label: "Ivory",
+    },
+    {
+      color: "#C0C0C0",
+      value: "Silver",
+      label: "Silver",
+    },
+    {
+      color: "#36454F",
+      value: "Charcoal",
+      label: "Charcoal",
+    },
+    // {
+    //   color: "",
+    //   value: "Latte",
+    //   label: "Latte",
+    // },
+    {
+      color: "#6F4E37",
+      value: "Coffee",
+      label: "Coffee",
+    },
+    {
+      color: "#A52A2A",
+      value: "Brown",
+      label: "Brown",
+    },
+    {
+      color: "#FFD700",
+      value: "Gold",
+      label: "Gold",
+    },
+    {
+      color: "#FFFF00",
+      value: "Yellow",
+      label: "Yellow",
+    },
+    {
+      color: "#FFA500",
+      value: "Orange",
+      label: "Orange",
+    },
+    {
+      color: "#b7410e",
+      value: "Rust",
+      label: "Rust",
+    },
+    {
+      color: "#FF0000",
+      value: "Red",
+      label: "Red",
+    },
+    {
+      color: "#501F2F",
+      value: "Merlot",
+      label: "Merlot",
+    },
+    {
+      color: "#FF69B4",
+      value: "Hot Pink",
+      label: "Hot Pink",
+    },
+    {
+      color: "#D5698A",
+      value: "Paris Pink",
+      label: "Paris Pink",
+    },
+    {
+      color: "#AE7876",
+      value: "Wood Rose",
+      label: "Wood Rose",
+    },
+    {
+      color: "#CBAFAA",
+      value: "Rose Water",
+      label: "Rose Water",
+    },
+    {
+      color: "#F4C2C2",
+      value: "Baby Pink",
+      label: "Baby Pink",
+    },
+    {
+      color: "#f88379",
+      value: "Coral",
+      label: "Coral",
+    },
+    {
+      color: "#E6E6FA",
+      value: "Lavender",
+      label: "Lavender",
+    },
+    {
+      color: "#9966CC",
+      value: "Amethyst",
+      label: "Amethyst",
+    },
+    {
+      color: "#000080",
+      value: "Navy",
+      label: "Navy",
+    },
+    {
+      color: "#4169E1",
+      value: "Royal Blue",
+      label: "Royal Blue",
+    },
+    {
+      color: "#00ffff",
+      value: "Aqua",
+      label: "Aqua",
+    },
+    {
+      color: "#5DBADB",
+      value: "Saltwater",
+      label: "Saltwater",
+    },
+    {
+      color: "#89CFF0",
+      value: "Baby Blue",
+      label: "Baby Blue",
+    },
+    {
+      color: "#bcb88a",
+      value: "Sage",
+      label: "Sage",
+    },
+    {
+      color: "#32cd32",
+      value: "Lime",
+      label: "Lime",
+    },
+    {
+      color: "#00cd00",
+      value: "Green",
+      label: "Green",
+    },
+    {
+      color: "#50c878",
+      value: "Emerald",
+      label: "Emerald",
+    },
+    {
+      color: "#355e3b",
+      value: "Hunter",
+      label: "Hunter",
+    },
+  ];
+
   // Default value for nameOutput
   let name = "Enter your Name";
-  let numberFound = false;
 
   // State for nameOutput
   const [nameOutput, setNameOutput] = useState(name);
 
   // State for currently selected font
   const [cF, setCf] = useState(currentFont[0]);
+
+  const [fontColor, setFontColor] = useState(currentColor[0]);
 
   // Changes font on Select Change
   const handleFontChange = (event) => {
@@ -191,20 +360,32 @@ export const Generator = () => {
     setCf(currentFont[fontIndex]);
   };
 
-
   // Changes nameOutput to inputed value
   const handleNameChange = (event) => {
     let newText = event.target.value;
-    console.log(newText);
     let globalText = "";
     const regex = /[^a-zA-Z\u002D\u0027]+/gi;
-    console.log("newtext 1: " + globalText);
 
     globalText = newText.replaceAll(regex, "");
-    console.log("First Regex: " + globalText);
 
     setNameOutput(globalText);
+  };
 
+  const handleColorChange = (event) => {
+    const newColor = event.value;
+
+    let colorIndex;
+
+    let changeColor = currentColor.filter((style, i) => {
+      if (style.value === newColor) {
+        colorIndex = i;
+        return;
+      }
+    });
+    console.log("fontcolor: " + fontColor.color);
+
+    setFontColor(currentColor[colorIndex]);
+    console.log("fontcolor2: " + fontColor.color);
   };
 
   return (
@@ -228,11 +409,21 @@ export const Generator = () => {
             onChange={handleFontChange}
             styles={{ backgroundColor: "#fffff0", align_self: "center" }}
           ></Select>
+          <Select
+            options={currentColor}
+            onChange={handleColorChange}
+            styles={{ backgroundColor: "#fffff0", color: currentColor.color }}
+          ></Select>
         </span>
       </div>
       <div className="split-right">
         {/* Changes div class to match with font-face css in index.css */}
-        <div className={cF.fontClass + " nameOutput"}>{nameOutput}</div>
+        <div
+          style={{ color: fontColor.color }}
+          className={cF.fontClass + " nameOutput"}
+        >
+          {nameOutput}
+        </div>
       </div>
     </div>
   );
