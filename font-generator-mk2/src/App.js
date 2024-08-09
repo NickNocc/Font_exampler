337bc00437434bacb077f3adbbfdb6faba16107e
NickNocc <haretakis4@gmail.com>
2024-08-09 12:43:23 -0700

    sdfas

--- font-generator-mk2/src/App.js
+++ font-generator-mk2/src/App.js
@@ -1,18 +1,180 @@
 import "./App.css";
-import React from "react";
+import React,  { useState } from "react";
 import { Generator } from "./components/generator";
 
 function App() {
+  const currentFont = [
+    //  {
+    //    fontName: "X",     Name Listed
+    //    fontClass: "X-X",  Connects to the font's css class
+    //    value: "X",        Im not sure honestly lmao
+    //    label: "X"         The label in the list
+    //  },
+    {
+      fontName: "Almond Butter",
+      fontClass: "font-face-Almondbutter",
+      value: "Almondbutter",
+      label: "Almond Butter",
+    },
+    {
+      fontName: "Amazingly beautiful",
+      fontClass: "font-face-Amazingly",
+      value: "Amazingly beautiful",
+      label: "Amazingly Beautiful",
+    },
+    {
+      fontName: "Andrea",
+      fontClass: "font-face-Andrea",
+      value: "Andrea",
+      label: "Andrea",
+    },
+    {
+      fontName: "Aurora",
+      fontClass: "font-face-aurora",
+      value: "Aurora",
+      label: "Aurora",
+    },
+    {
+      fontName: "Autery",
+      fontClass: "font-face-Autery",
+      value: "Autery",
+      label: "Autery",
+    },
+    {
+      fontName: "Baby Point",
+      fontClass: "font-face-Babypoint",
+      value: "Babypoint",
+      label: "Baby Point",
+    },
+    {
+      fontName: "Baked Fresh",
+      fontClass: "font-face-Bakedfresh",
+      value: "Bakedfresh",
+      label: "Baked Fresh",
+    },
+    {
+      fontName: "Blossom",
+      fontClass: "font-face-blossom",
+      value: "Blossom",
+      label: "Blossom",
+    },
+    {
+      fontName: "Boho Flowers",
+      fontClass: "font-face-BohoFlowers",
+      value: "BohoFlowers",
+      label: "BohoFlowers",
+    },
+    {
+      fontName: "Chakra",
+      fontClass: "font-face-Chakra",
+      value: "Chakra",
+      label: "Chakra",
+    },
+    {
+      fontName: "Chalk",
+      fontClass: "font-face-Chalk",
+      value: "Chalk",
+      label: "Chalk",
+    },
+    {
+      fontName: "Classico",
+      fontClass: "font-face-classico",
+      value: "classico",
+      label: "Classico",
+    },
+    {
+      fontName: "Clocks",
+      fontClass: "font-face-Clocks",
+      value: "Clocks",
+      label: "Clocks",
+    },
+    {
+      fontName: "Cookie",
+      fontClass: "font-face-Cookie",
+      value: "Cookie",
+      label: "Cookie",
+    },
+    {
+      fontName: "Doodle",
+      fontClass: "font-face-Doodle",
+      value: "Doodle",
+      label: "Doodle",
+    },
+    {
+      fontName: "Eduarda",
+      fontClass: "font-face-Eduarda",
+      value: "Eduarda",
+      label: "Eduarda",
+    },
+    {
+      fontName: "Ghisella",
+      fontClass: "font-face-Ghisella",
+      value: "Ghisella",
+      label: "Ghisella",
+    },
+    {
+      fontName: "Hancock",
+      fontClass: "font-face-Hancock",
+      value: "Hancock",
+      label: "Hancock",
+    },
+    {
+      fontName: "Harpsichord",
+      fontClass: "font-face-Harpsichord",
+      value: "Harpsichord",
+      label: "Harpsichord",
+    },
+    {
+      fontName: "Langrish",
+      fontClass: "font-face-Langrish",
+      value: "Langrish",
+      label: "Langrish",
+    },
+    {
+      fontName: "Layla",
+      fontClass: "font-face-Layla",
+      value: "Layla",
+      label: "Layla",
+    },
+    {
+      fontName: "Lively",
+      fontClass: "font-face-Lively",
+      value: "Lively",
+      label: "Lively",
+    },
+    {
+      fontName: "Sweetheart",
+      fontClass: "font-face-Sweetheart",
+      value: "Sweetheart",
+      label: "Sweetheart",
+    },
+    {
+      fontName: "Tatertot",
+      fontClass: "font-face-Tatertot",
+      value: "Tatertot",
+      label: "Tatertot",
+    },
+    {
+      fontName: "Violetta",
+      fontClass: "font-face-Violetta",
+      value: "Violetta",
+      label: "Violetta",
+    },
+  ];
+  const [cF, setCf] = useState(currentFont[0]);
+
   return (
     <div className="App">
       <header className="App-header font-face-Brittany">
         Baby Boo's Font Generator
       </header>
       {/* Loads Generator conponent */}
-      <Generator />
-      <footer
-        className="disclaimer"
-      >
+      <Generator 
+        currentFont={currentFont}
+        setcf={setCf}
+        cF={cF}
+      />
+      <footer className="disclaimer">
         {" "}
         * Please note: Not all fonts will register numbers, special characters,
         etc. Sizing and setup of letters is approximate. If you have any
--- font-generator-mk2/src/components/generator.js
+++ font-generator-mk2/src/components/generator.js
@@ -1,168 +1,14 @@
 import React, { useState } from "react";
-import Popup from "reactjs-popup";
 import Select from "react-select";
 import "./Generator.css";
 
-export const Generator = () => {
+export const Generator = (props) => {
+
+  const { currentFont } = props
+  const cF = props;
+  const setCf = props;
   // List of fonts Loaded onto the website
-  const currentFont = [
-    //  {
-    //    fontName: "X",     Name Listed
-    //    fontClass: "X-X",  Connects to the font's css class
-    //    value: "X",        Im not sure honestly lmao
-    //    label: "X"         The label in the list
-    //  },
-    {
-      fontName: "Almond Butter",
-      fontClass: "font-face-Almondbutter",
-      value: "Almondbutter",
-      label: "Almond Butter",
-    },
-    {
-      fontName: "Amazingly beautiful",
-      fontClass: "font-face-Amazingly",
-      value: "Amazingly beautiful",
-      label: "Amazingly Beautiful",
-    },
-    {
-      fontName: "Andrea",
-      fontClass: "font-face-Andrea",
-      value: "Andrea",
-      label: "Andrea",
-    },
-    {
-      fontName: "Aurora",
-      fontClass: "font-face-aurora",
-      value: "Aurora",
-      label: "Aurora",
-    },
-    {
-      fontName: "Autery",
-      fontClass: "font-face-Autery",
-      value: "Autery",
-      label: "Autery",
-    },
-    {
-      fontName: "Baby Point",
-      fontClass: "font-face-Babypoint",
-      value: "Babypoint",
-      label: "Baby Point",
-    },
-    {
-      fontName: "Baked Fresh",
-      fontClass: "font-face-Bakedfresh",
-      value: "Bakedfresh",
-      label: "Baked Fresh",
-    },
-    {
-      fontName: "Blossom",
-      fontClass: "font-face-blossom",
-      value: "Blossom",
-      label: "Blossom",
-    },
-    {
-      fontName: "Boho Flowers",
-      fontClass: "font-face-BohoFlowers",
-      value: "BohoFlowers",
-      label: "BohoFlowers",
-    },
-    {
-      fontName: "Chakra",
-      fontClass: "font-face-Chakra",
-      value: "Chakra",
-      label: "Chakra",
-    },
-    {
-      fontName: "Chalk",
-      fontClass: "font-face-Chalk",
-      value: "Chalk",
-      label: "Chalk",
-    },
-    {
-      fontName: "Classico",
-      fontClass: "font-face-classico",
-      value: "classico",
-      label: "Classico",
-    },
-    {
-      fontName: "Clocks",
-      fontClass: "font-face-Clocks",
-      value: "Clocks",
-      label: "Clocks",
-    },
-    {
-      fontName: "Cookie",
-      fontClass: "font-face-Cookie",
-      value: "Cookie",
-      label: "Cookie",
-    },
-    {
-      fontName: "Doodle",
-      fontClass: "font-face-Doodle",
-      value: "Doodle",
-      label: "Doodle",
-    },
-    {
-      fontName: "Eduarda",
-      fontClass: "font-face-Eduarda",
-      value: "Eduarda",
-      label: "Eduarda",
-    },
-    {
-      fontName: "Ghisella",
-      fontClass: "font-face-Ghisella",
-      value: "Ghisella",
-      label: "Ghisella",
-    },
-    {
-      fontName: "Hancock",
-      fontClass: "font-face-Hancock",
-      value: "Hancock",
-      label: "Hancock",
-    },
-    {
-      fontName: "Harpsichord",
-      fontClass: "font-face-Harpsichord",
-      value: "Harpsichord",
-      label: "Harpsichord",
-    },
-    {
-      fontName: "Langrish",
-      fontClass: "font-face-Langrish",
-      value: "Langrish",
-      label: "Langrish",
-    },
-    {
-      fontName: "Layla",
-      fontClass: "font-face-Layla",
-      value: "Layla",
-      label: "Layla",
-    },
-    {
-      fontName: "Lively",
-      fontClass: "font-face-Lively",
-      value: "Lively",
-      label: "Lively",
-    },
-    {
-      fontName: "Sweetheart",
-      fontClass: "font-face-Sweetheart",
-      value: "Sweetheart",
-      label: "Sweetheart",
-    },
-    {
-      fontName: "Tatertot",
-      fontClass: "font-face-Tatertot",
-      value: "Tatertot",
-      label: "Tatertot",
-    },
-    {
-      fontName: "Violetta",
-      fontClass: "font-face-Violetta",
-      value: "Violetta",
-      label: "Violetta",
-    },
-  ];
+
   // List of colors loaded onto the site
   const currentColor = [
     // {
@@ -351,14 +197,13 @@ export const Generator = () => {
   const [nameOutput, setNameOutput] = useState(name);
 
   // State for currently selected font
-  const [cF, setCf] = useState(currentFont[0]);
 
   // State for currently selected font color
   const [fontColor, setFontColor] = useState(currentColor[0]);
   // State for currentely selected minkyBack color
   const [minkyBack, setMinkyBack] = useState(currentMinkyBack[0]);
   // State for modal toggle
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   // Changes font on Select Change
   const handleFontChange = (event) => {
@@ -372,6 +217,8 @@ export const Generator = () => {
         console.log(currentFont[i]);
         fontIndex = i;
         return;
+      } else{
+        setCf(currentFont[0])
       }
     });
     // Sets the current font to the index of selected font
@@ -417,7 +264,7 @@ export const Generator = () => {
     });
   };
   const handleModalChange = (event) => {
-    console.log(cF);
+    console.log(currentFont[0]);
 
     setIsOpen((isOpen) => !isOpen);
   };
