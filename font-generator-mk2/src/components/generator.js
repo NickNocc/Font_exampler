face196947c6ae522c194d8874dd904dafaf339d
Nick Haretakis <haretakis4@gmail.com>
2025-03-07 09:40:11 -0800

    Update generator.js

--- font-generator-mk2/src/components/generator.js
+++ font-generator-mk2/src/components/generator.js
@@ -1256,6 +1256,7 @@ export const Generator = () => {
       <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
     </svg>
   );
+  
   let [firstBreak, setFirstBreak] = useState(false);
 
   // Changes font on Select Change
