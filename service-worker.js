d4d8dda9bbfca9cc3dc89f2b627024bffa9a467d
Nick Haretakis <haretakis4@gmail.com>
2025-05-02 13:38:29 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -187,7 +187,6 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log("installing : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
